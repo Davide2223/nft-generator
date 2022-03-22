00945559e355a8e2cf630641665f8cb20092f60b
Davide2223 <94624314+Davide2223@users.noreply.github.com>
2022-03-22 23:11:52 +0100

    Update hash_generate.py

--- hash_generate.py
+++ hash_generate.py
@@ -1,22 +1,27 @@
-from hashlib import sha256
-import os
+from hashlib import sha256 
+import os 
 import pygame
 
-hash_matches = {}
-list_dilay = os.listdir('dilay')
+hash_matches = {} #dict used for register hash of any img
+list_dilay = os.listdir('dilay') #list used for register any directory of imgs
 
+#function used for generate hash for nft
 def generate_hash_nft(hash_img):
-    unicode = hash_img
+    unicode = hash_img 
     hash = sha256(unicode.encode('utf-8')).hexdigest()
-    return hash
     
+    return hash
+
+#function uded for generate hash for img
 def generate_hash_img(name, size, width, height):
     unicode = name + str(size) + str(width) + str(height)
     hash = sha256(unicode.encode('utf-8')).hexdigest()
+    
     return hash
- 
+
+#update dict with hash of any img 
 for dilay in list_dilay:
     for img in os.listdir(f'dilay/{dilay}'):
-        width, height = pygame.image.load(f'dilay/{dilay}/{img}').get_rect()[2:]
+        width, height = pygame.image.load(f'dilay/{dilay}/{img}').get_rect()[2:] #using pygame module for get width and height of img
         hash_matches.update({img: generate_hash_img(img, pygame.image.load(f'dilay/{dilay}/{img}').get_size(), width, height)})
 
